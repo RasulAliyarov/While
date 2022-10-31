035e4a3069471d2608f0bdc62b588ac9f1a01fe8
RasulAliyarov <rasulaliyarov2002@gmail.com>
2022-11-01 03:42:17 +0400

    lesson files

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿//int sum = int.Parse(Console.ReadLine());
+//int d = 0;
+//for (int i = 1; i <= sum; i++)
+//{
+//    d = d + i;
+//}
+//Console.WriteLine(d);
+
+//---------------------------------------------------------------------
+
+
+
+int number = 100;
+
+
+for(int i = 1; i <= number; i++)
+{
+    if (i % 3 == 0 || i % 5 == 0)
+    {
+        Console.WriteLine(i);
+    }
+    
+}
