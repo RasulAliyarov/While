fbf9fd551ebbf3a7f499d8785bc894873574e3bf
RasulAliyarov <rasulaliyarov2002@gmail.com>
2022-11-03 01:28:13 +0400

    add js file

--- main.js
+++ main.js
@@ -1,11 +1,32 @@
-let number = 100;
+////////Massiv
+let numbers = [1, 2, 3, 4];
 
+console.log(numbers)
 
-for(let i = 1; i <= number; i++)
-{
-    if (i % 3 == 0 || i % 5 == 0)
-    {
-        console.log(i);
-    }
-    
+for (let i = 0; i < numbers.length; i++) {
+    console.log(numbers[i] + 1)
 }
+
+/////Functions
+
+function sum(a, b) {
+    return a + b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function substract(a, b) {
+    return a - b;
+}
+function divide(a, b) {
+    return a / b;
+}
+
+let result = sum(5,5);
+let result2 = multiply(5,5);
+let result3 = substract(5,5)
+let result4= divide(5,5);
+
+console.log('Sum: '+result, 'Multiply: '+result2, 'Substract: '+result3, 'Divide: '+result4);
